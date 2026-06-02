1d2736ac2736ed5241a8dbfdc81e654a1fb08ab8
Rama <ramirologiurato@gmail.comgit config --global user.name Ramagit statusgit config --global user.email ramirologiurato@gmail.com2>
2026-06-01 23:13:24 -0300

    CSS and JS finished, ready to upload.

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+const form = document.querySelector("form");
+const emailInput = document.getElementById("emailInput");
+const submitButton = document.getElementById("submitButton");
+const popUp = document.querySelector("article");
+const dismissButton = document.getElementById("dismissButton");
+const errorMsg = document.getElementById("errorLabel");
 
+function handleSubmit(e) {
+  e.preventDefault();
+}
+
+form.addEventListener("submit", handleSubmit);
+
+function hideForm() {
+  popUp.style.opacity = "0";
+  popUp.style.zIndex = "-2";
+  form.style.opacity = "1";
+}
+
+dismissButton.addEventListener("click", hideForm);
+
+submitButton.addEventListener("click", function () {
+  if (!emailInput.validity.valid) {
+    emailInput.classList.add("erroremail");
+    errorMsg.style.opacity = "1";
+  } else {
+    form.style.opacity = "0";
+    popUp.style.zIndex = "2";
+    popUp.style.opacity = "1";
+  }
+});
